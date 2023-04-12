90abbb889a4f116fae2b0ed63ff8c6598e7de6a7
John Smith <su13122000@gmai.com>
2023-04-12 21:07:07 +0700

    General(NgFor_NgIf)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,13 @@ export class HomeComponent implements OnInit {
 
   public name = 'Su';
   public age = 23;
-  public juices = ['Apple', 'Orange', 'Banana', 'Lemon', 'Tomato'];
+  public juices = [
+    { name: 'Apple', price: 10000, quantity: -10, discount: true },
+    { name: 'Orange', price: 15000, quantity: 50 , discount: false},
+    { name: 'Lemon', price: 5000, quantity: -50, discount: true   },
+    { name: 'Kiwi', price: 20000, quantity: 50 , discount: false},
+    { name: 'Tomato', price: 3000, quantity: 50 , discount: false},
+  ];
 
   constructor() { }
 
